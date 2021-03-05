705c5094b1241d30173928c341b60e2c242950d1
Pat Kinzler <pat.kinzler@kellogg.northwestern.edu>
2021-03-05 17:24:34 -0600

    V2

--- addmeal.js
+++ addmeal.js
@@ -1,17 +1,19 @@
 firebase.auth().onAuthStateChanged(async function(user) {
     if (user) {
-      let userName = firebase.auth().currentUser.displayName
-          document.querySelector('.userName').innerHTML = `
-              <a href="#" class="welcome-sign italic text-4xl font-semibold">Welcome ${userName}</a>
-          `
-          document.querySelector('.sign-in-or-sign-out').innerHTML = `
-              <a href="#" class="sign-out-button underline >Sign Out</a>
-          `
-          document.querySelector('.sign-out-button').addEventListener('click', function(event) {
-              event.preventDefault()
-              firebase.auth().signOut() 
-              document.location.href = 'signin.html'   
-          })
+        let db = firebase.firestore()
+        let userName = firebase.auth().currentUser.displayName
+        document.querySelector('.userName').innerHTML = `
+            <a href="#" class="welcome-sign italic text-4xl font-semibold">Welcome ${userName}</a>
+        `
+        document.querySelector('.sign-in-or-sign-out').innerHTML = `
+            <a href="#" class="sign-out-button underline >Sign Out</a>
+        `
+        document.querySelector('.sign-out-button').addEventListener('click', function(event) {
+            event.preventDefault()
+            firebase.auth().signOut() 
+            document.location.href = 'signin.html'   
+        })
+
   
       // Signed in
       console.log('signed in')
