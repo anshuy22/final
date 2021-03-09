8562283a8b27192f1b3facec76ffc9c189b554da
Pat Kinzler <pat.kinzler@kellogg.northwestern.edu>
2021-03-08 23:55:37 -0600

    V4

--- addmeal.js
+++ addmeal.js
@@ -8,16 +8,60 @@ firebase.auth().onAuthStateChanged(async function(user) {
         document.querySelector('.sign-in-or-sign-out').innerHTML = `
             <a href="#" class="sign-out-button underline >Sign Out</a>
         `
+
+        document.querySelector('form').addEventListener('click', async function(event) {
+          event.preventDefault()
+
+          let mealName = document.querySelector('.mealName').value
+          // let ingredientOneName = document.querySelector('.ingredientOne').value
+          // let ingredientTwoName = document.querySelector('.ingredientTwo').value
+          // let ingredientThreeName = document.querySelector('.ingredientThree').value
+
+          console.log(mealName)
+          // console.log(ingredientOneName)
+          // console.log(ingredientTwoName)
+          // console.log(ingredientThreeName)
+
+          // if (mealName.length > 0) {
+          //   let docRef = await db.collection('Meals').add({
+          //     text: mealName
+          //   })
+          // }
+
+          // if (ingredientOneName.length > 0) {
+          //   let docRef = await db.collection('ingredients').add({
+          //     text: ingredientOneName
+          //   })
+          // }
+
+          // if (ingredientTwoName.length > 0) {
+          //   let docRef = await db.collection('ingredients').add({
+          //     text: ingredientTwoName
+          //   })
+          // }
+
+          // if (ingredientThreeName.length > 0) {
+          //   let docRef = await db.collection('ingredients').add({
+          //     text: ingredientThreeName
+          //   })
+          // }
+        })
+
+
         document.querySelector('.sign-out-button').addEventListener('click', function(event) {
             event.preventDefault()
             firebase.auth().signOut() 
-            document.location.href = 'signin.html'   
-        })
-
-  
+            document.location.href = 'signin.html'
+        }) 
+       
+      
       // Signed in
       console.log('signed in')
     } else {
+
+      document.querySelector('form').classList.add('hidden')
+      document.querySelector('#links').classList.add('hidden')
+
       // Signed out
       console.log('signed out')
   
